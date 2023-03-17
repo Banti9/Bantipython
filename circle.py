81b58b6ff6c3f23dbd8c4397b7d6bde5c371bc0c
Banti jk <110710466+Banti9@users.noreply.github.com>
2023-03-17 18:07:17 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,14 @@
+import turtle as t
+t.speed(0)
+t.bgcolor("black")
+t.color("violetred")
+
+t.up()
+t.goto(90,0)
+t.down()
+for i in range(900):
+    t.circle(i)
+    t.rt(1)
+    t.ht()
+    
+t.done()    
